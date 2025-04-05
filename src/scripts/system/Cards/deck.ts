d821fbb8c2104dcf55863e7116764a26acd40e91
Niemand1337 <soejan@outlook.de>
2025-04-05 19:28:39 +0200

    Defined a playing class as interface. Created deck.ts with different functions to manage the cards of a player. Added empty player class and the utitity file with a shuffle function

---
+++ src/scripts/system/Cards/deck.ts
@@ -0,0 +1,48 @@
+import { shuffleArray } from "../../utility";
+import { Card } from "./card"
+
+export class Deck {
+    draw_pile: Card[]; // Cards get drawn from here. If empty mix the discard pile into it
+    discard_pile: Card[]; // Pile of used cards
+    hand: Card[] // The cards the player can use
+
+    constructor(draw_pile: Card[], discard_pile: Card[] = []) {
+        this.draw_pile = shuffleArray(draw_pile); // The start deck of the player
+        this.discard_pile = shuffleArray(discard_pile); // Default is a empty discard pile, but may be changed by effects
+        this.hand = []; // Hand is always empty at the begin
+    }
+
+    /**
+     * Removes the given cards from the hand and puts them into the discard_pile
+     * @param cards - The Cards that should be played from the hand
+     */
+    play_from_hand(cards: Card[]) {
+        this.discard_pile = [...this.discard_pile, ...cards]; // Combines the played cards with the discard_pile
+        cards.forEach(card => {
+            this.hand.splice(this.hand.indexOf(card), 1); // Removes card from hand
+        })
+    }
+
+    /**
+     * Removes x cards from the draw_pile and adds them to the hand 
+     * @param x The number of cards that should be drawn from the draw_pile
+     */
+    draw_from_draw_pile(x: number) {
+        for (let i = 0; i < x; i++) {
+            if (this.draw_pile.length) { // If draw_pile is not empty
+                this.hand.push(this.draw_pile.pop() as Card) // Removes last card from the draw_pile and pushes it into the hand
+                continue;
+            }
+            this.discard_into_draw();
+            i--; // No card was added
+        }
+    }
+
+    /**
+     * Mixes all cards from the discard pile into the draw pile and shuffles it
+     */
+    discard_into_draw() {
+        this.draw_pile = shuffleArray([...this.draw_pile, ...this.discard_pile]); // Combines draw and discard pile and shuffles afterwards
+        this.discard_pile = []; // All cards from the discard pile are now in the draw pile
+    }
+}
---
+++ src/scripts/utility.ts
@@ -0,0 +1,14 @@
+
+/**
+ * Shuffels the given array around and returns it
+ * Implementation of the Fisher-Yates-Algorithmus
+ * @param array - The array that will be shuffeld
+ * @returns - The array with elements in randomised order
+ */
+export function shuffleArray<T>(array: T[]): T[] {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
