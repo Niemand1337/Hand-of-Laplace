6a14c592e8b98e862289fc6b1d4d48880c84bae2
Niemand1337 <soejan@outlook.de>
2025-04-06 16:41:34 +0200

    Made enemy more versitile

--- src/scripts/system/Enemy/enemy.ts
+++ src/scripts/system/Enemy/enemy.ts
@@ -1,3 +1,5 @@
+import { Ability } from "./ability";
+
 export interface Enemy {
     name: String; // Display name
     current_health: number // Dead if 0
@@ -5,4 +7,5 @@ export interface Enemy {
     damage: number // On hit damage
     current_speed: number // Counts down, attacks on 0 and resets to maximal_speed
     maximal_speed: number // Start value of current_speed
+    abilitys: Ability[]; // Abilities the enemy has (example, half damage each second attack)
 }
