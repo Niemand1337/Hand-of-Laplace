cf5010c09db4128de4350c4671cd8a22d71aea01
Niemand1337 <soejan@outlook.de>
2025-04-06 10:20:33 +0200

    Added Stats to the Player interface

---
+++ src/scripts/system/Character/skilltree.ts
@@ -0,0 +1,7 @@
+import { LevelSystem } from "./levelSystem";
+
+export interface Skilltree {
+    type: String; // The name of the class the player uses
+    level_system: LevelSystem; // The management of the player level
+
+}
--- src/scripts/system/player.ts
+++ src/scripts/system/player.ts
@@ -1,7 +1,9 @@
 import { Deck } from "./Cards/deck"
-import { Skilltree } from "./Level/skilltree"
+import { Skilltree } from "./Character/skilltree"
+import { Stats } from "./Character/stats"
 
 export interface Player {
-    skilltree: Skilltree, // How the player used his level
-    deck: Deck // The Cards of the player
+    skilltree: Skilltree; // How the player used his level
+    stats: Stats; // The values of the player, like health and hand size
+    deck: Deck; // The cards of the player
 }
