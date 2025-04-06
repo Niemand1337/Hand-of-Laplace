4a68888b4466ae49a021672bc28f7c79bf89b3a9
Niemand1337 <soejan@outlook.de>
2025-04-06 12:21:44 +0200

    Add enemy interface

---
+++ src/scripts/system/enemy.ts
@@ -0,0 +1,8 @@
+export interface Enemy {
+    name: String; // Display name
+    current_health: number // Dead if 0
+    maximal_health: number // current_health can't be higher
+    damage: number // On hit damage
+    current_speed: number // Counts down, attacks on 0 and resets to maximal_speed
+    maximal_speed: number // Start value of current_speed
+}
