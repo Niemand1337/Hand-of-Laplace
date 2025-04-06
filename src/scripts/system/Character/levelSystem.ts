682aa72be0451d9259bfe22e1fb834ffc832621c
Niemand1337 <soejan@outlook.de>
2025-04-06 10:57:35 +0200

    Added levelSystem class

--- src/scripts/system/Character/levelSystem.ts
+++ src/scripts/system/Character/levelSystem.ts
@@ -1,3 +1,37 @@
 export class LevelSystem {
-    
+    level: number; // The current level of the player
+    maximal_xp: number; // The xp where the level up happens
+    current_xp: number; // The xp the player currently has
+
+    constructor(level: number = 0, maximal_xp: number = 100, current_xp: number = 0) {
+        this.level = level;
+        this.maximal_xp = maximal_xp;
+        this.current_xp = current_xp;
+    }
+
+    /**
+     * Add xp to the player, returns true on level up
+     * @param x - The amount of xp the player receives
+     * @returns true on level up, else false
+     */
+    change_current_xp(x: number): boolean {
+        this.current_xp += x;
+        if(this.current_xp >= this.maximal_xp) { // Level up
+            this.current_xp -= this.maximal_xp;
+            this.level++;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Add x to the maximal_xp to change it.
+     * Checks for level up (possible on x < 0) and returns true on it
+     * @param x - Value to change, can be negative
+     * @returns true on level up, else false
+     */
+    change_maximal_xp(x: number): boolean {
+        this.maximal_xp += x;
+        return this.change_current_xp(0); // Checks for level up on negative values
+    }
 }
--- src/scripts/system/Character/skilltree.ts
+++ src/scripts/system/Character/skilltree.ts
@@ -5,5 +5,4 @@ export interface Skilltree {
     type: String; // The name of the class the player uses
     level_system: LevelSystem; // The management of the player level
     skills: Skill[]; // All the skills the player selected
-
 }
