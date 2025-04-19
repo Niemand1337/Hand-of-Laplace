008143caf8999c7771a6fa09dfb43b3d531733a7
Niemand1337 <soejan@outlook.de>
2025-04-19 19:04:55 +0200

    Implemented the abstract add function for the abstract class EvolutionSkill implemented from the abstract class Skill

--- src/scripts/system/Character/Skill/SkillType/evolvingSkill.ts
+++ src/scripts/system/Character/Skill/SkillType/evolvingSkill.ts
@@ -1,7 +1,10 @@
 import { Skill } from "../skills";
 
-// Skills that evolve. Can completly change and become a new skill, if certain skills already exist 
-// The certain skills to replace are needed while adding, but most evolving skills have the same evolution and groups of replaces
+/**
+ * Skills that evolve. Can completly change and become a new skill, if certain skills already exist
+ * Can only evolve with skills of the same skillType
+ * The certain skills to replace are needed while adding, but most evolving skills have the same evolution and groups of replaces
+ */
 export default abstract class EvolvingSkill extends Skill {
     skillClass: number = 3; // EvolutionSkills are class 3
     replaces: String[]; // The skills needed to evolve
@@ -11,5 +14,34 @@ export default abstract class EvolvingSkill extends Skill {
         super(skillType, name, value);
         this.replaces = replaces;
         this.evolution = evolution;
+    }
+
+    add(skills: Skill[]): void {
+        if (this.evolution_criteria(skills)) {
+            this.replaces.forEach(skill_name => { // Removes the skills used for the evolution from the given Skill[]
+                skills.forEach(skill => {
+                    if (skill.name === skill_name) {
+                        skill.remove(skills);
+                    }
+                });
+            });
+            this.evolution.add(skills); // Add the evolution skill
+            return;
+        }
+        skills.push(this); // Evolution criteria didn't met, add the skill
+    }
+
+    /**
+     * Returns if all skills to replace for evolution are there
+     * @param skills - The skills of the same skillType
+     * @returns - If the skill should evolve
+     */
+    private evolution_criteria(skills: Skill[]): boolean {
+        this.replaces.forEach(skill_name => {
+            if (skills.findIndex(skill => { skill.name === skill_name;}) === -1) { // Skill to replace dosn't exist
+                return false;
+            }
+        });
+        return true
     }
 }
