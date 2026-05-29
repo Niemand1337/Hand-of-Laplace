6fe41bb815b2c41123ffc3071cd640032d52ee0a
Niemand1337 <soejan@outlook.de>
2026-05-29 23:32:06 +0200

    Changed skilltree from interface to abstract class

--- src/scripts/system/Character/Skill/skilltree.ts
+++ src/scripts/system/Character/Skill/skilltree.ts
@@ -2,8 +2,17 @@ import { LevelNode } from "../Level/levelNode";
 import { LevelSystem } from "../Level/levelSystem";
 import { SkillManager } from "./skillManager";
 
-export interface Skilltree {
+export default abstract class Skilltree {
     level_system: LevelSystem; // The management of the player level
     skills: SkillManager; // All the skills the player selected
-    tree: LevelNode; // The node of the tree (Contains a skill and the following nodes) 
+    tree: LevelNode; // The node of the tree (Contains a skill and the following nodes)
+    type: String; // The class of the player, like "Mage"
+
+    constructor(type: String, tree: LevelNode) {
+        this.level_system = new LevelSystem();
+        this.skills = new SkillManager();
+        this.type = type;
+        this.tree = tree;
+        
+    }
 }
